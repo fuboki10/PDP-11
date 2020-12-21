be5cf7221c32c318ce7b837355697ce7c67f6f76
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-12-21 19:48:51 +0200

    oneOperand Done

--- src/assembler/main.cpp
+++ src/assembler/main.cpp
@@ -11,14 +11,26 @@ using namespace std;
   std::cin.tie(NULL);                    \
   std::cout.tie(NULL);
 
+template <size_t N1, size_t N2>
+bitset<N1 + N2> concat(const bitset<N1> &b1, const bitset<N2> &b2)
+{
+  string s1 = b1.to_string();
+  string s2 = b2.to_string();
+  return bitset<N1 + N2>(s1 + s2);
+}
+
 // ==================================
 vector<string> code;
 map<string, int> labels, variables;
 map<string, bitset<16>> noOperand;
+map<string, bitset<10>> oneOperand;
 // ===================================
 
 // ==============Functions============
 bitset<16> compile(string line);
+
+bitset<3> getMode(string &inst);
+bitset<3> getRegister(string &inst);
 
 string fetchInst(string &line);
 void removeLabel(string &line);
@@ -36,6 +48,7 @@ string getLabelName(string &s);
 bool isLabel(string &s);
 bool isVariable(string &s);
 bool isNoOperand(string &inst);
+bool isOneOperand(string &inst);
 
 // ====================================
 
@@ -45,6 +58,17 @@ void init()
   noOperand["HLT"] = bitset<16>("0000000000000000");   // 000000
   noOperand["NOP"] = bitset<16>("0000000010100000");   // 000240
   noOperand["RESET"] = bitset<16>("0000000000000101"); // 000005
+
+  // oneOperand init
+  oneOperand["INC"] = bitset<10>("0000101010"); // 0052
+  oneOperand["DEC"] = bitset<10>("0000101011"); // 0053
+  oneOperand["CLR"] = bitset<10>("0000101000"); // 0050
+  oneOperand["INV"] = bitset<10>("0000101001"); // 0051
+  oneOperand["LSR"] = bitset<10>("0000101111"); // 0057
+  oneOperand["ROR"] = bitset<10>("0000110000"); // 0060
+  oneOperand["ASR"] = bitset<10>("0000110010"); // 0062
+  oneOperand["LSL"] = bitset<10>("0000110011"); // 0063
+  oneOperand["ROL"] = bitset<10>("0000110001"); // 0061
 }
 
 int main(int argc, char **argv)
@@ -144,6 +168,17 @@ bitset<16> compile(string line)
     return noOperand[instName];
   }
 
+  // one operand
+  if (isOneOperand(instName))
+  {
+    bitset<10> opCode = oneOperand[instName];
+    string dst = fetchInst(line);
+    bitset<3> mode = getMode(dst);
+    bitset<3> reg = getRegister(dst);
+    bitset<16> code = concat(opCode, concat(mode, reg));
+    return code;
+  }
+
   return skip;
 }
 // ====================================
@@ -166,6 +201,11 @@ bool isVariable(string &s)
 bool isNoOperand(string &inst)
 {
   return noOperand.count(inst) > 0;
+}
+
+bool isOneOperand(string &inst)
+{
+  return oneOperand.count(inst) > 0;
 }
 
 string toUpperCase(string &s)
@@ -251,13 +291,71 @@ string getLabelName(string &s)
 
 string fetchInst(string &line)
 {
+  removeSpacesAndTabs(line);
   string inst = "";
   int i = 0;
   while (i < line.size() && line[i] != ' ' && line[i] != '\t')
     inst += line[i++];
-  cout << inst << endl;
   line.erase(0, i);
   return inst;
+}
+
+bitset<3> getMode(string &inst)
+{
+  bitset<3> mode;
+  if (inst[0] == 'R')
+    mode = 0;
+  else if (inst[0] == '-')
+    mode = 4;
+  else if (inst[0] == '(')
+    mode = 2;
+  else if (isdigit(inst[0]))
+    mode = 6;
+  else if (inst[0] == '@')
+  {
+    if (inst[1] == 'R')
+      mode = 1;
+    else if (inst[1] == '-')
+      mode = 5;
+    else if (inst[1] == '(')
+      mode = 3;
+    else if (isdigit(inst[1]))
+      mode = 7;
+    else
+    {
+      cerr << "Compilation Error at : " << inst << endl;
+      exit(1);
+    }
+  }
+  else
+  {
+    cerr << "Compilation Error at : " << inst << endl;
+    exit(1);
+  }
+
+  return mode;
+}
+
+bitset<3> getRegister(string &inst)
+{
+  bitset<3> reg;
+  int i = 0;
+  while (i < inst.size() && inst[i] != 'R')
+    i++;
+  if (i + 1 >= inst.size())
+  {
+    cerr << "Compilation Error at : " << inst << endl;
+    exit(1);
+  }
+  char num = inst[i + 1];
+  if (num >= '0' && num <= '7')
+    reg = num - '0';
+  else
+  {
+    cerr << "Compilation Error at : " << inst << endl;
+    exit(1);
+  }
+  return reg;
 }
 
 void removeLabel(string &line)
