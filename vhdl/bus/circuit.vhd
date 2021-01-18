074c93eaa208018d45295a60678390528c5de0c1
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-01-18 02:17:34 +0200

    add data buss and alu

--- vhdl/bus/circuit.vhd
+++ vhdl/bus/circuit.vhd
@@ -9,8 +9,13 @@ END circuit;
 ARCHITECTURE circuit_arch OF circuit IS
 
 signal inverted_src_enable, inverted_dest_enable : std_logic;
-signal reg0_out, reg1_out, reg2_out, reg3_out, reg4_out, reg5_out, reg6_out, reg7_out, ram_out, bus_line : std_logic_vector(15 downto 0);
+signal reg0_out, reg1_out, reg2_out, reg3_out, reg4_out, reg5_out, reg6_out, reg7_out, mdr_out, mar_out, ir_out, temp_out, y_out, z_out, flag_out, ram_out, bus_line, a_in, b_in : std_logic_vector(15 downto 0);
 signal dest_out, src_out : std_logic_vector(7 downto 0);
+signal en : std_logic_vector(6 downto 0);
+signal tri_en : std_logic_vector(5 downto 0);
+
+signal alu_flag_in, alu_out, alu_flag_out: std_logic_vector(16 downto 0);
+signal alu_s: std_logic_vector(3 downto 0);
 
 component my_nDFF IS
 GENERIC (n : integer := 32);
@@ -44,6 +49,15 @@ component ram IS
 		dataout : OUT std_logic_vector(N-1 DOWNTO 0));
 END component;
 
+component alu IS
+	GENERIC (n : INTEGER := 16);
+	PORT (
+		a, b, flag : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
+		s : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		flagout, f : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0));
+
+END component;
+
 signal counter_out : std_logic_vector(10 downto 0);
 BEGIN
 	PROCESS (clk, rst)
@@ -58,6 +72,10 @@ BEGIN
               	END IF;
         	counter_out <= std_logic_vector(to_unsigned(cnt,11));
        	END PROCESS;
+
+
+	alu_label: alu GENERIC MAP(16) port map(a_in, b_in, alu_flag_in, alu_s, alu_flag_out, alu_out); 
+		
 	src_label: ndecoder GENERIC MAP(3) port map(src_enable, src_sel, src_out);
 	dest_label: ndecoder GENERIC MAP(3) port map(dest_enable, dest_sel, dest_out);
 	reg0_label: my_nDFF GENERIC MAP(16) port map(clk, rst, dest_out(0), bus_line, reg0_out);
@@ -68,6 +86,14 @@ BEGIN
 	reg5_label: my_nDFF GENERIC MAP(16) port map(clk, rst, dest_out(5), bus_line, reg5_out);
 	reg6_label: my_nDFF GENERIC MAP(16) port map(clk, rst, dest_out(6), bus_line, reg6_out);
 	reg7_label: my_nDFF GENERIC MAP(16) port map(clk, rst, dest_out(7), bus_line, reg7_out);
+
+	mdr_label: my_nDFF GENERIC MAP(16) port map(clk, rst, en(0), bus_line, mdr_out);
+	mar_label: my_nDFF GENERIC MAP(16) port map(clk, rst, en(1), bus_line, mar_out);
+	ir_label: my_nDFF GENERIC MAP(16) port map(clk, rst, en(2), bus_line, ir_out); 
+	temp_label: my_nDFF GENERIC MAP(16) port map(clk, rst, en(3), bus_line, temp_out); 
+	y_label: my_nDFF GENERIC MAP(16) port map(clk, rst, en(4), bus_line, y_out); 
+	z_label: my_nDFF GENERIC MAP(16) port map(clk, rst, en(5), alu_out, z_out); 
+	flag_label: my_nDFF GENERIC MAP(16) port map(clk, rst, en(6), alu_flag_out, flag_out); 
 
 	ram_label: ram GENERIC MAP(16,11) port map(clk, inverted_dest_enable, counter_out, bus_line, ram_out);
 
@@ -83,4 +109,14 @@ BEGIN
 	tristate6_label: tristate_buffer GENERIC MAP(16) port map(src_out(6), reg6_out, bus_line);
 	tristate7_label: tristate_buffer GENERIC MAP(16) port map(src_out(7), reg7_out, bus_line);
 	tristate8_label: tristate_buffer GENERIC MAP(16) port map(inverted_src_enable, ram_out, bus_line);
+
+	tristate9_label: tristate_buffer GENERIC MAP(16) port map(tri_en(0), mdr_out, bus_line);
+	tristate10_label: tristate_buffer GENERIC MAP(16) port map(tri_en(1), temp_out, bus_line);
+	tristate11_label: tristate_buffer GENERIC MAP(16) port map(tri_en(2), z_out, bus_line);
+	tristate12_label: tristate_buffer GENERIC MAP(16) port map(tri_en(3), flag_out, bus_line);
+
+	tristate13_label: tristate_buffer GENERIC MAP(16) port map(tri_en(4), bus_line, b_in);
+	tristate14_label: tristate_buffer GENERIC MAP(16) port map(tri_en(5), y_out, a_in);
+	
+
 END circuit_arch;
